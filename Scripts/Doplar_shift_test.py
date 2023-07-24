6340c5158c8f235d3ae559713d1bbf6ccb3310e4
george burns <lp21819@bristol.ac.uk>
2023-07-24 16:28:17 +0100

    Better test file for doppler

--- Scripts/Doplar_shift_test.py
+++ Scripts/Doplar_shift_test.py
@@ -1,4 +1,38 @@
-def Get_orbital(line_1,line_2,lat,long,acc,height,start_elv):
+def Get_orbital(line_1,line_2,lat,long,height,start_elv, acc = 100):
+    """
+    Predicts the next orbital pass for a sattellite given a location and height 
+
+    Parameters
+    ----------
+    line_1 : string
+        First line of TLE data.
+    line_2 : string
+        Second line of TLE data.
+    lat : float
+        lattitude of ground station in degrees.
+    long : float
+        Longitude of ground station in degrees.
+    acc : int
+        accuracy of calcs.
+    height : int or float
+        height of ground station in m.
+    start_elv : float
+        elevation angle to start observation in degrees.
+
+    Returns
+    -------
+    list
+        Datetime object of next closet window and how long until.
+    list
+        Duration of window and end date of window .
+    elv : list
+        Elevation data of the pass.
+    d : list
+        Slant range data of the pass.
+    time_window : list
+        time data of pass.
+
+    """
     from TLE import TLE_calc
     import numpy as np
     import datetime
@@ -10,11 +44,7 @@ def Get_orbital(line_1,line_2,lat,long,acc,height,start_elv):
     sec = 0
     step = 0.1
     
-    
-    
-    
-    
-    
+
     time_now = datetime.datetime.now(timezone.utc)
     while (np.rad2deg(elv1) < start_elv):
         sat = TLE_calc(line_1, line_2,True,time_now.year,time_now.month,time_now.day,time_now.hour,time_now.minute,
@@ -30,7 +60,7 @@ def Get_orbital(line_1,line_2,lat,long,acc,height,start_elv):
 
     current_elv = elv1
     time_window = []
-    elv = []
+    elv = [];Az = []
     d = []
     sec = 0
     step = step/10
@@ -39,7 +69,7 @@ def Get_orbital(line_1,line_2,lat,long,acc,height,start_elv):
         sat = TLE_calc(line_1, line_2,True,time_now.year,time_now.month,time_now.day,time_now.hour,time_now.minute,
                       time_now.second,time_now.microsecond)
         d_current,Az1, current_elv,q = sat.get_pos_TOPO(acc,lat,long,height)
-        elv.append(current_elv);d.append(d_current);time_window.append(date_at_start+datetime.timedelta(seconds=sec))
+        elv.append(current_elv);d.append(d_current);time_window.append(date_at_start+datetime.timedelta(seconds=sec));Az.append(Az1)
         sec = sec+step
         
         
@@ -47,7 +77,7 @@ def Get_orbital(line_1,line_2,lat,long,acc,height,start_elv):
     
     time_till_end_of_window = datetime.timedelta(seconds=sec)
     date_at_end = date_at_start+datetime.timedelta(seconds=sec)
-    return [time_till_window,date_at_start],[time_till_end_of_window,date_at_end], elv,d,time_window
+    return [time_till_window,date_at_start],[time_till_end_of_window,date_at_end], elv,d,time_window, Az
 
 def Oribtal_time_check():
     import numpy as np
@@ -96,11 +126,21 @@ def Oribtal_time_check():
         
     f.close()
 
-
-
-
-
 def Get_range_rate(d):
+    """
+    Gets the range rate change
+
+    Parameters
+    ----------
+    d : list
+        Range rate in Km*-.
+
+    Returns
+    -------
+    range_rate : list
+        range rate change in Km.
+
+    """
     import numpy as np
     range_rate = np.diff(d,1)
     
@@ -114,15 +154,21 @@ def Get_range_rate(d):
             if range_rate[i] == 0:
                 range_rate[i] = range_rate[i-1]
     
+
+    
+    range_rate = range_rate.tolist()
+    
+    range_rate.append(range_rate[len(range_rate)-1])
+    
     return range_rate
     
-
-
-
-
-
-
-
+def Get_freq_change(range_rate):
+    freq_change = []
+    for i in range(len(range_rate)):
+        freq_change.append((3E8)/(3E8+(range_rate[i]*1000)))
+        
+    return freq_change
+        
 def Doppler_shift_test():
     import numpy as np
     import datetime
@@ -148,6 +194,60 @@ def Doppler_shift_test():
 
     plt.plot((3E8)/(3E8+(range_rate*1000)))
     
+def All_togther_now_test():
+    import matplotlib.pyplot as plt
+    import matplotlib.dates as mdates
+    import numpy as np
+    # Data for the ISS and lat/long and height for Ham Street kent 
+    line_1 = "1 25544U 98067A   23203.12476910  .00011930  00000-0  21722-3 0  9991"
+    line_2 = '2 25544  51.6411 155.4137 0000523  72.9869 282.0755 15.49927938407206'
+    lat = 51.00653
+    long = 0.85587541
+    height = 0
+    start_elv = 1
+    Pass_info = Get_orbital(line_1, line_2, lat, long, height, start_elv)
+    Start_date = Pass_info[0]
+    End_date = Pass_info[1]
+    elv = Pass_info[2]
+    d = Pass_info[3]
+    time_window = Pass_info[4]
+    Az = Pass_info[5]
 
+    range_rate = Get_range_rate(d)
+
+    f_change = Get_freq_change(range_rate)
+    
+    
+    
+    
+    
+    fig1, (sub1, sub2,sub3) = plt.subplots(3,1, figsize=(10, 10))
+    sub1.plot(time_window,np.rad2deg(elv))
+    sub2.plot(time_window,f_change)
+    sub3.plot(time_window,np.rad2deg(Az))
+
+    
+    
+    sub1.xaxis.set_major_formatter(mdates.DateFormatter('%M:%S'))
+    sub2.xaxis.set_major_formatter(mdates.DateFormatter('%M:%S'))
+    sub3.xaxis.set_major_formatter(mdates.DateFormatter('%M:%S'))
+    sub1.grid(True);sub2.grid(True);sub3.grid(True)
+    
+    
+    
+    
+    
+    sub1.set_ylabel("Degrees")
+    sub2.set_ylabel("Change in freq")
+    sub3.set_ylabel("Degrees")
+    sub3.set_xlabel("Time")
+    sub1.set_title("Elevation angle")
+    sub2.set_title("Frequency change")
+    sub3.set_title("Azmith Angle (wrong)")
+    format1 = "%Y/%m/%d %H:%M:%S"
+    fig1.suptitle((f"Data for next ISS pass in {Start_date[0]} at {Start_date[1].strftime(format1)} (UTC)"),
+                  verticalalignment = 'center', fontsize = 'xx-large', y= 0.93)
+    plt.show()
+    
 if __name__ == "__main__":
-    Doppler_shift_test()
+    All_togther_now_test()
